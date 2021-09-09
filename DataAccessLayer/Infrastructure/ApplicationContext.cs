07a1321dc54e3cb568632d01b35693b75a3200b6
Adeel Ahmed <adeel.ahmed@iengineering.com>
2021-09-10 01:29:05 +0500

    initial commit

---
+++ DataAccessLayer/Infrastructure/ApplicationContext.cs
@@ -0,0 +1,20 @@
+﻿using DataAccessLayer.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Infrastructure
+{
+    public class ApplicationContext : IdentityDbContext<User>
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+
+        }
+        public DbSet<ToDoList> ToDoLists { get; set; }
+    }
+}
---
+++ ToDoClientApp/Controllers/SecurityController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoClientApp.Controllers
+{
+    public class SecurityController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(string username)
+        {
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> Signup()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Signup(string userModel)
+        {
+            return View();
+        }
+    }
+}
