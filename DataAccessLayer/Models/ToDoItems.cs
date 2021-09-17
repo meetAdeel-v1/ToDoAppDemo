9de796814896d485625e0ead177324435c27aa7f
Adeel Ahmed <adeel.ahmed@iengineering.com>
2021-09-18 03:00:53 +0500

    Added ToDoList items model.

---
+++ DataAccessLayer/Models/ToDoItems.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Models
+{
+    public class ToDoItems
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [ForeignKey("ToDoList")]
+        public int ToDoID { get; set; }
+
+    }
+}
